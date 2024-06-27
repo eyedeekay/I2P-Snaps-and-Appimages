8eb1329c8ef5c2142b745b3eb12638e1962728b2
eyedeekay <hankhill19580@gmail.com>
2024-06-27 02:17:49 -0400

    use launcher to workaround unwritable config files

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
 if [ ! -d "$SNAP_USER_DATA/config" ]; then
-    cp -rv "$SNAP/usr/share/i2p-bundle/lib/config" "$SNAP_USER_DATA/config"
+    cp -rv "$SNAP/usr/share/i2p-bundle/lib/config" "$SNAP_USER_DATA/i2p-config"
 fi
 /usr/share/i2p-bundle/bin/I2P-BUNDLE

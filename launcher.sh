ae737b1f9aa228511718b11776c89816e623d06d
eyedeekay <hankhill19580@gmail.com>
2024-06-27 10:26:46 -0400

    Fix path in launcher

--- launcher.sh
+++ launcher.sh
@@ -2,4 +2,4 @@
 if [ ! -d "$SNAP_USER_DATA/config" ]; then
     cp -rv "$SNAP/usr/share/i2p-bundle/lib/config" "$SNAP_USER_DATA/i2p-config"
 fi
-/usr/share/i2p-bundle/bin/I2P-BUNDLE
+"$SNAP/usr/share/i2p-bundle/bin/I2P-BUNDLE"

2088271a6a0195ae6a67c8bb0dabea64a7090d18
eyedeekay <hankhill19580@gmail.com>
2024-07-09 19:23:33 -0400

    AppImage, launcher fix

--- appimage.sh
+++ appimage.sh
@@ -15,6 +15,7 @@ echo building launcher
 ./buildscripts/launcher.sh
 cp ../launcher.sh src/I2P/launcher.sh
 cp LICENSE.md src
+rm -rf I2P-BUNDLE-APPIMAGE
 jpackage --name I2P-BUNDLE-APPIMAGE --app-version "$I2P_VERSION" \
     --verbose \
     --java-options "-Xmx512m" \
@@ -34,3 +35,21 @@ jpackage --name I2P-BUNDLE-APPIMAGE --app-version "$I2P_VERSION" \
     --type app-image \
     --main-jar launcher.jar \
     --main-class net.i2p.router.WinLauncher
+rm -rf ../i2pAppDir
+cp -rv I2P-BUNDLE-APPIMAGE ../i2pAppDir
+cd ../i2pAppDir || exit 1
+cp lib/windowsUIToopie2.png i2p.png
+echo "[Desktop Entry]" | tee i2p.desktop
+echo "Name=i2p" | tee -a i2p.desktop
+echo "Exec=i2p" | tee -a i2p.desktop
+echo "Icon=i2p" | tee -a i2p.desktop
+echo "Type=Application" | tee -a i2p.desktop
+echo "Categories=Network;" | tee -a i2p.desktop
+#ln -s bin/I2P-BUNDLE-APPIMAGE AppRun || exit 1
+echo "#! /usr/bin/env sh" | tee AppRun
+echo "if [ ! -d \"$HOME/i2p-config\" ]; then" | tee -a AppRun
+echo "   cp -rv \"lib/config" "$HOME/i2p-config\"" | tee -a AppRun
+echo fi | tee -a AppRun
+echo "./bin/I2P-BUNDLE-APPIMAGE" | tee -a AppRun
+chmod +x AppRun
+"$HOME/Downloads/appimagetool-x86_64.AppImage" . ../i2p.AppImage
--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/env sh
-if [ ! -d "$SNAP_USER_DATA/config" ]; then
+if [ ! -d "$SNAP_USER_DATA/i2p-config" ]; then
     cp -rv "$SNAP/usr/share/i2p-bundle/lib/config" "$SNAP_USER_DATA/i2p-config"
 fi
 "$SNAP/usr/share/i2p-bundle/bin/I2P-BUNDLE"
